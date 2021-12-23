61db10f1eafa3d8f77144a13fbe3da892d16ee90
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-12-23 23:46:41 +0700

    refactor

--- app/src/main/java/com/hoc081098/stateflowsample/MainVM.kt
+++ app/src/main/java/com/hoc081098/stateflowsample/MainVM.kt
@@ -13,10 +13,6 @@ import kotlinx.coroutines.flow.*
 @FlowPreview
 @ExperimentalCoroutinesApi
 class MainVM : ViewModel() {
-  private companion object {
-    const val MAX_SECONDS = 10
-  }
-
   private val actionChannel = Channel<MainAction>(Channel.BUFFERED)
 
   val stateFlow: StateFlow<MainState>
@@ -42,11 +38,7 @@ class MainVM : ViewModel() {
       .flatMapLatest { action ->
         when (action) {
           MainAction.START -> {
-            generateSequence(resume + 1) { it + 1 }
-              .asFlow()
-              .onEach { delay(1_000) }
-              .onStart { emit(resume) }
-              .takeWhile { it <= MAX_SECONDS }
+            tickerFlow(resume)
               .map {
                 MainState(
                   seconds = it,
@@ -63,14 +55,24 @@ class MainVM : ViewModel() {
               )
             )
           }
-          MainAction.RESET -> {
-            flowOf(initial)
-          }
+          MainAction.RESET -> flowOf(initial)
         }
       }
       .onEach { stateFlow.value = it }
       .onEach { Log.d("###", "Main state: $it") }
       .catch { Log.d("###", "Exception: $it") }
       .launchIn(viewModelScope)
+  }
+
+  private fun tickerFlow(resume: Long): Flow<Long> {
+    return generateSequence(resume + 1) { it + 1 }
+      .asFlow()
+      .onEach { delay(1_000) }
+      .onStart { emit(resume) }
+      .takeWhile { it <= MAX_SECONDS }
+  }
+
+  private companion object {
+    const val MAX_SECONDS = 10
   }
 }
